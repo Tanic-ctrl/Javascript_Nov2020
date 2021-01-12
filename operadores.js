24cc1d4a4ee40f14ef838e454bd913c08f9c6089
“Tania” <tania@photoshelter.com>
2021-01-11 22:17:48 -0600

    Se agrego Burbujas, condicionantes, loops

---
+++ operadores.js
@@ -0,0 +1,55 @@
+var a = 1;
+var b = 2;
+var c = 3;
+
+console.log("Resultado:", a + b)
+console.log("Resultado:", a, b)
+console.log("Resultado:", c - b)
+console.log("Resultado:", a * b)
+console.log("Resultado:", a / c)
+console.log("Resultado:", 98 % 2)
+console.log("Resultado:", a++)
+console.log("Resultado:", a--)
+
+console.log("Cadena", 879 + "3")
+console.log("Cadena 1" + "Cadena 2")
+console.log(2 + 2 + "3")
+
+const cinco = 5
+const diez = 10
+console.log("Quince es: " + (cinco + diez) + " y no: " + (2 * diez + 5) + '.')
+
+//Tarea
+console.log("...Tarea operadores...\nEjemplo:")
+var num = 8
+num = num / 2
+console.log(num)
+console.log("Resultado división")
+num = 8
+console.log(num /= 2)
+
+//Usemos los siguientes operadores como ejemplos
+console.log("Operadores como ejemplos:")
+    //*=
+console.log("Resultado multiplicación")
+num1 = 5
+num2 = 7
+console.log(num1 *= num2)
+
+//-=
+console.log("Resultado resta")
+num3 = 4.5
+num4 = 6.7
+console.log(num4 -= num3)
+
+//+=
+console.log("Resultado suma")
+num5 = 33
+num6 = 99
+console.log(num5 += num6)
+
+//%=
+console.log("Resultado residual")
+num7 = 98
+num8 = 2
+console.log(num7 %= num8)
